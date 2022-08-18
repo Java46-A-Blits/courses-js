472454e60ed9a62613aa9d0045f2916a04a88ace
Alex Blits <blits.alex@gmail.com>
2022-08-18 17:47:13 +0300

    CW22

---
+++ src/utils/random.js
@@ -0,0 +1,10 @@
+export function getRandomNumber(min, max){
+    //TODO - returs a random number in the range min to max
+}
+export function getRandomElement(array){
+    // TODO  - returns a random element of an array
+}
+export function getRandomDate (minYear, maxYear){
+    // TODO  - returns random date object ( see constructor of the standard cass Date)
+    const date  = new Date(year, month, day)
+}
---
+++ src/utils/randomCourse.js
@@ -0,0 +1,7 @@
+import { createCourse } from "../models/course";
+export function getRandomCourse(courseData){
+    //TODO - random  id, date opening, name of lecturer etc ...
+    //getting random aruments for the below function call
+    return createCourse(id,  name, lecturer, hours, cost, openingDate)
+    
+}
